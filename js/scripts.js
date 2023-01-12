7b30eb8e3c97d846f035df08adc6904a6d8fbb60
NicksFed <nick.guzy76@gmail.com>
2023-01-12 11:57:44 -0800

    Add calculator div
    Co-authored-by: Mike Donovan <mike@mikedonovan.dev>

--- js/scripts.js
+++ js/scripts.js
@@ -30,15 +30,14 @@ function divide(firstNumber, secondNumber){
 
 function calculate(firstNumber, secondNumber, operator) {
   let result = '';
-    if (operator === "+") {
-      result = add(firstNumber, secondNumber);
-    } else if (operator === "-") {
-      result = subtract(firstNumber, secondNumber);
-    } else if (operator === "×") {
-      result = multiply(firstNumber, secondNumber);
-    } else if (operator === "÷") {
-      result = divide(firstNumber, secondNumber);
-    }
-    
-    return result;
+  if (operator === "+") {
+    result = add(firstNumber, secondNumber);
+  } else if (operator === "-") {
+    result = subtract(firstNumber, secondNumber);
+  } else if (operator === "×") {
+    result = multiply(firstNumber, secondNumber);
+  } else if (operator === "÷") {
+    result = divide(firstNumber, secondNumber);
+  }
+  return result;
 }
