15a3e0f181db17fae8ec47f2697d8c2ac59ef260
NicksFed <nick.guzy76@gmail.com>
2023-01-12 13:45:44 -0800

    Add calculator functionality
    Co-authored-by: Mike Donovan <mike@mikedonovan.dev>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,55 @@
+const operations = {
+  currentNumber: undefined,
+  operator: undefined,
+  total: 0
+}
+
+function resetOperations() {
+  operations.currentNumber = undefined;
+  operations.operator = undefined;
+  operations.total = undefined;
+}
+
 // UI logic
 
 window.onload = function(){
-  document.querySelector("form").onsubmit=function(e){
-    e.preventDefault()
-    let firstNumber = parseInt(document.getElementById("number1").value);
-    let secondNumber = parseInt(document.getElementById("number2").value);
-    let operator = document.getElementById("operator-select").value;
-    calculate(firstNumber, secondNumber, operator);
-  }
+  // document.querySelector("form").onsubmit=function(e){
+  //   e.preventDefault()
+  //   let firstNumber = parseInt(document.getElementById("number1").value);
+  //   let secondNumber = parseInt(document.getElementById("number2").value);
+  //   let operator = document.getElementById("operator-select").value;
+  //   calculate(firstNumber, secondNumber, operator);
+  // }
+
+  let allButtons = document.getElementsByClassName('calculator-button');
+
+  Array.from(allButtons).forEach(function(button) {
+    button.onclick = function(e) {
+      let buttonLabel = e.target.innerHTML;
+      if (e.target.id === 'clear-button') {
+        document.getElementById('calculator-display').innerText = "";
+        resetOperations();
+      } else if (e.target.classList.contains('operator')) {
+        document.getElementById('calculator-display').innerText = "";
+        operations.operator = buttonLabel;
+        if (!operations.total) {
+          operations.total = operations.currentNumber;
+          operations.currentNumber = undefined;
+        }
+      } else if (e.target.id === "equals-button") {
+        if (operations.currentNumber && operations.operator && operations.total) {
+          let result = calculate(operations.total, operations.currentNumber, operations.operator);
+          document.getElementById('calculator-display').innerText = result;
+          operations.total = result;
+        }
+      } else {
+        document.getElementById('calculator-display').innerText += buttonLabel;
+        operations.currentNumber = parseInt(document.getElementById('calculator-display').innerText);
+        
+      }
+      console.table(operations);
+    }
+  });
 }
 
 // Business logic
